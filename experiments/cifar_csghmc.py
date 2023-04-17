2a301fbdcdaf231799ae1605a353072426f76541
Sahra Ghalebikesabi <s.ghalebikesabi@gmail.com>
2023-04-17 12:31:06 +0100

    make example run on cuda without using GPUs

--- experiments/cifar_csghmc.py
+++ experiments/cifar_csghmc.py
@@ -1,7 +1,8 @@
-'''Train CIFAR10 with PyTorch.'''
+"""Train CIFAR10 with PyTorch."""
 from __future__ import print_function
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -19,21 +20,36 @@ from torch.autograd import Variable
 import numpy as np
 import random
 
-parser = argparse.ArgumentParser(description='cSG-MCMC CIFAR10 Training')
-parser.add_argument('--dir', type=str, default=None, required=True, help='path to save checkpoints (default: None)')
-parser.add_argument('--data_path', type=str, default=None, required=True, metavar='PATH',
-                    help='path to datasets location (default: None)')
-parser.add_argument('--epochs', type=int, default=200,
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                    help='input batch size for training (default: 64)')
-parser.add_argument('--alpha', type=float, default=0.9,
-                    help='1: SGLD; <1: SGHMC')
-parser.add_argument('--device_id',type = int, help = 'device id to use')
-parser.add_argument('--seed', type=int, default=1,
-                    help='random seed')
-parser.add_argument('--temperature', type=float, default=1./50000,
-                    help='temperature (default: 1/dataset_size)')
+parser = argparse.ArgumentParser(description="cSG-MCMC CIFAR10 Training")
+parser.add_argument(
+    "--dir", type=str, default="ckpt", help="path to save checkpoints (default: None)"
+)
+parser.add_argument(
+    "--data_path",
+    type=str,
+    default="data",
+    metavar="PATH",
+    help="path to datasets location (default: None)",
+)
+parser.add_argument(
+    "--epochs", type=int, default=200, help="number of epochs to train (default: 10)"
+)
+parser.add_argument(
+    "--batch-size",
+    type=int,
+    default=64,
+    metavar="N",
+    help="input batch size for training (default: 64)",
+)
+parser.add_argument("--alpha", type=float, default=0.9, help="1: SGLD; <1: SGHMC")
+parser.add_argument("--device_id", type=int, help="device id to use")
+parser.add_argument("--seed", type=int, default=1, help="random seed")
+parser.add_argument(
+    "--temperature",
+    type=float,
+    default=1.0 / 50000,
+    help="temperature (default: 1/dataset_size)",
+)
 
 args = parser.parse_args()
 device_id = args.device_id
@@ -43,56 +59,81 @@ np.random.seed(args.seed)
 random.seed(args.seed)
 
 # Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+print("==> Preparing data..")
+transform_train = transforms.Compose(
+    [
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ]
+)
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+transform_test = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ]
+)
 
-trainset = torchvision.datasets.CIFAR10(root=args.data_path, train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=0)
+trainset = torchvision.datasets.CIFAR10(
+    root=args.data_path, train=True, download=True, transform=transform_train
+)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=args.batch_size, shuffle=True, num_workers=0
+)
 
-testset = torchvision.datasets.CIFAR10(root=args.data_path, train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=0)
+testset = torchvision.datasets.CIFAR10(
+    root=args.data_path, train=False, download=True, transform=transform_test
+)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=0
+)
 
 # Model
-print('==> Building model..')
+print("==> Building model..")
 net = ResNet18()
+
+use_cuda = False
 if use_cuda:
     net.cuda(device_id)
     cudnn.benchmark = True
     cudnn.deterministic = True
 
-def update_params(lr,epoch):
+
+def update_params(lr, epoch):
     for p in net.parameters():
-        if not hasattr(p,'buf'):
-            p.buf = torch.zeros(p.size()).cuda(device_id)
+        if not hasattr(p, "buf"):
+            if use_cuda:
+                p.buf = torch.zeros(p.size()).cuda(device_id)
+            else:
+                p.buf = torch.zeros(p.size())
         d_p = p.grad.data
         d_p.add_(weight_decay, p.data)
-        buf_new = (1-args.alpha)*p.buf - lr*d_p
-        if (epoch%50)+1>45:
-            eps = torch.randn(p.size()).cuda(device_id)
-            buf_new += (2.0*lr*args.alpha*args.temperature/datasize)**.5*eps
+        buf_new = (1 - args.alpha) * p.buf - lr * d_p
+        if (epoch % 50) + 1 > 45:
+            if use_cuda:
+                eps = torch.randn(p.size()).cuda(device_id)
+            else:
+                eps = torch.randn(p.size())
+            buf_new += (
+                2.0 * lr * args.alpha * args.temperature / datasize
+            ) ** 0.5 * eps
         p.data.add_(buf_new)
         p.buf = buf_new
 
+
 def adjust_learning_rate(epoch, batch_idx):
-    rcounter = epoch*num_batch+batch_idx
+    rcounter = epoch * num_batch + batch_idx
     cos_inner = np.pi * (rcounter % (T // M))
     cos_inner /= T // M
     cos_out = np.cos(cos_inner) + 1
-    lr = 0.5*cos_out*lr_0
+    lr = 0.5 * cos_out * lr_0
     return lr
 
+
 def train(epoch):
-    print('\nEpoch: %d' % epoch)
+    print("\nEpoch: %d" % epoch)
     net.train()
     train_loss = 0
     correct = 0
@@ -101,20 +142,28 @@ def train(epoch):
         if use_cuda:
             inputs, targets = inputs.cuda(device_id), targets.cuda(device_id)
         net.zero_grad()
-        lr = adjust_learning_rate(epoch,batch_idx)
+        lr = adjust_learning_rate(epoch, batch_idx)
         outputs = net(inputs)
         loss = criterion(outputs, targets)
         loss.backward()
-        update_params(lr,epoch)
+        update_params(lr, epoch)
 
         train_loss += loss.data.item()
         _, predicted = torch.max(outputs.data, 1)
         total += targets.size(0)
         correct += predicted.eq(targets.data).cpu().sum()
-        if batch_idx%100==0:
-            print('Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                % (train_loss/(batch_idx+1), 100.*correct.item()/total, correct, total))
-    
+        if batch_idx % 100 == 0:
+            print(
+                "Loss: %.3f | Acc: %.3f%% (%d/%d)"
+                % (
+                    train_loss / (batch_idx + 1),
+                    100.0 * correct.item() / total,
+                    correct,
+                    total,
+                )
+            )
+
+
 def test(epoch):
     global best_acc
     net.eval()
@@ -133,29 +182,42 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets.data).cpu().sum()
 
-            if batch_idx%100==0:
-                print('Test Loss: %.3f | Test Acc: %.3f%% (%d/%d)'
-                    % (test_loss/(batch_idx+1), 100.*correct.item()/total, correct, total))
+            if batch_idx % 100 == 0:
+                print(
+                    "Test Loss: %.3f | Test Acc: %.3f%% (%d/%d)"
+                    % (
+                        test_loss / (batch_idx + 1),
+                        100.0 * correct.item() / total,
+                        correct,
+                        total,
+                    )
+                )
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-    test_loss/len(testloader), correct, total,
-    100. * correct.item() / total))
+    print(
+        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n".format(
+            test_loss / len(testloader), correct, total, 100.0 * correct.item() / total
+        )
+    )
+
 
 weight_decay = 5e-4
 datasize = 50000
-num_batch = datasize/args.batch_size+1
-lr_0 = 0.5 # initial lr
-M = 4 # number of cycles
-T = args.epochs*num_batch # total number of iterations
+num_batch = datasize / args.batch_size + 1
+lr_0 = 0.5  # initial lr
+M = 4  # number of cycles
+T = args.epochs * num_batch  # total number of iterations
 criterion = nn.CrossEntropyLoss()
 mt = 0
 
+print("==> Starting training..")
 for epoch in range(args.epochs):
     train(epoch)
     test(epoch)
-    if (epoch%50)+1>47: # save 3 models per cycle
-        print('save!')
-        net.cpu()
-        torch.save(net.state_dict(),args.dir + '/cifar_csghmc_%i.pt'%(mt))
+    if (epoch % 50) + 1 > 47:  # save 3 models per cycle
+        print("save!")
+        if use_cuda:
+            net.cpu()
+        torch.save(net.state_dict(), args.dir + "/cifar_csghmc_%i.pt" % (mt))
         mt += 1
-        net.cuda(device_id)
+        if use_cuda:
+            net.cuda(device_id)
